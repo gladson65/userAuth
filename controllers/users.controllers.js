0e5639f733137da6fdace777aa77e03a5f518732
gladson65 <grauth65@gmail.com>
2025-10-23 17:06:54 +0530

    register function has been created

---
+++ controllers/users.controllers.js
@@ -0,0 +1,44 @@
+import userModel from "../models/users.model.js";
+
+// sign up or register function
+export async function register(req, res) {
+
+    // destructuring from the req body
+    const { username, email, password } = req.body;
+
+    try {
+        // key validation
+        if (!username, !email, !password) return res.status(400).json({message: "check all fields like username, email, password are coming or not"});
+        
+        // field validation
+        if (username.length < 5) return res.status(400).json({message: "username must be 5 characters"});
+        if (email) {
+            let test = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+            if (!test) return res.status(400).json({message: 'Invalid email format'});
+        }
+        if (password) {
+            let test = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/.test(password);
+            if (!test) return res.status(400).json({message: 'Password must contain one digit from 1 to 9, one lowercase letter, one uppercase letter, one special character, no space, and it must be 8-16 characters long.'})
+        }
+
+        // check email is already is in the db or not
+        let checkUser = await userModel.find({email: email});
+        if (checkUser) return res.status(400).json({message: 'User already exist, try with another email'});
+
+        // if a user is new
+        const newUser = new userModel({
+            username,
+            email,
+            password
+        })
+
+        // save the new user
+        await newUser.save();
+        return res.status(201).json({message: 'user has been registered successfully'}); 
+    
+    }
+    catch(error) {
+        return res.status(500).json({error: error.message});
+    }
+    
+}
