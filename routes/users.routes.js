b020c79e3ee597ececc7839355e3e4a934c43a38
gladson65 <grauth65@gmail.com>
2025-10-23 18:39:18 +0530

    auth middleware has been added on get all users route

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,9 +1,10 @@
 import { register, login, getAllUsers } from "../controllers/users.controllers.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
 
 function userRouter(userServer) {
     userServer.post("/register", register),
     userServer.post("/login", login),
-    userServer.get("/users", getAllUsers)
+    userServer.get("/users", verifyToken, getAllUsers)
 }
 
 export default userRouter;
